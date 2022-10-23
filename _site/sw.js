48864afb1360f79ab85469c0799262f3eab2bb85
sobrienti <sobrienti@icloud.com>
2022-10-23 17:03:24 -0500

    move

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221023.170130';
+const cacheName = 'chirpy-20221023.170320';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
